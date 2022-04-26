dbd1474013c6a79cbd6ce8c6530ee23898037283
Josh Goebel <me@joshgoebel.com>
2022-04-26 10:03:37 -0400

    wip

--- src/languages/purebasic.js
+++ src/languages/purebasic.js
@@ -50,6 +50,7 @@ export default function(hljs) {
 
       { // PROCEDURES DEFINITIONS
         className: 'function',
+        relevance: 1,
         begin: '\\b(Procedure|Declare)(C|CDLL|DLL)?\\b',
         end: '\\(',
         excludeEnd: true,
